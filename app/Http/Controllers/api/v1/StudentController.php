adff7d73ec0c23665573e149b85f3240a441dc43
James Hogg <hoggjt2@student.op.ac.nz>
2021-11-21 17:57:05 +1300

    edited student controller, create

--- app/Http/Controllers/api/v1/StudentController.php
+++ app/Http/Controllers/api/v1/StudentController.php
@@ -36,9 +36,9 @@ class StudentController extends Controller
 
     public function store(StoreStudentRequest $request)
     {
-        //return Student::create($request->all());
-        Student::create($request->validated());
-        return $this->response_message('Student created', 201);
+        return Student::create($request->all());
+        //Student::create($request->validated());
+        //return $this->response_message('Student created', 201);
     }
 
     public function show($id)
