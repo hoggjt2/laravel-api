3ef6d03da481532ecc09ebcbeab73d3ea3a61557
James Hogg <hoggjt2@student.op.ac.nz>
2021-11-21 13:19:30 +1300

    return id in student resource

--- app/Http/Resources/StudentResource.php
+++ app/Http/Resources/StudentResource.php
@@ -16,6 +16,7 @@ class StudentResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'first_name' => $this->first_name,
             'last_name' => $this->last_name,
             'institution_id' => $this->institution_id
